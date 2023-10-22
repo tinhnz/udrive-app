6b88aa161ad6faac07843bc4f027b1eede7911da
tinhtt <tinhtrantrung.vn@gmail.com>
2023-10-22 15:21:46 +1300

    Implement SearchForm.vue.

---
+++ resources/js/Components/app/Navigation.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="min-w-[200px]">
+        <div class="h-[80px] px-3 flex items-center gap-3">
+            <Link href="/">
+                <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800"/>
+            </Link>
+            UDrive
+        </div>
+        <div class="px-3">
+            <CreateNewDropdown/>
+
+            <div class="py-3">
+                <NavLink href="/">My Files</NavLink>
+                <NavLink href="/">Shared with me</NavLink>
+                <NavLink href="/">Shared by me</NavLink>
+                <NavLink href="/">Trash</NavLink>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import NavLink from '@/Components/NavLink.vue';
+import CreateNewDropdown from '@/Components/app/CreateNewDropdown.vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/js/Components/app/UserSettingsDropdown.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        UserSettingsDropdown
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
