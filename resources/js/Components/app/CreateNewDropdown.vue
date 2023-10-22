eb73c7717bd8f426d838de3a21a6a3a1d1c09d7e
tinhtt <tinhtrantrung.vn@gmail.com>
2023-10-22 15:53:15 +1300

    Implement CreateNew component.

--- resources/js/Components/app/CreateNewDropdown.vue
+++ resources/js/Components/app/CreateNewDropdown.vue
@@ -1,13 +1,48 @@
 <template>
-    <div>
-        CreateNewDropdown
-    </div>
+    <Menu as="div" class="relative block text-left">
+        <MenuButton
+            class="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+        >
+            Create New
+            <PlusIcon
+                class="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
+                aria-hidden="true"
+            />
+        </MenuButton>
+
+        <transition
+            enter-active-class="transition duration-100 ease-out"
+            enter-from-class="transform scale-95 opacity-0"
+            enter-to-class="transform scale-100 opacity-100"
+            leave-active-class="transition duration-75 ease-in"
+            leave-from-class="transform scale-100 opacity-100"
+            leave-to-class="transform scale-95 opacity-0"
+        >
+            <MenuItems
+                class="absolute left-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+            >
+                <div class="px-1 py-1">
+                    <MenuItem v-slot="{ active }">
+                        <a class="block px-4 py-2 text-sm"> New Folder </a>
+                    </MenuItem>
+                </div>
+                <div class="px-1 py-1">
+                    <MenuItem v-slot="{ active }">
+                        <a class="block px-4 py-2 text-sm"> Upload Files </a>
+                    </MenuItem>
+                    <MenuItem v-slot="{ active }">
+                        <a class="block px-4 py-2 text-sm"> Upload Folders </a>
+                    </MenuItem>
+                </div>
+            </MenuItems>
+        </transition>
+    </Menu>
 </template>
 
 <script setup>
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+import { PlusIcon } from "@heroicons/vue/20/solid";
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
